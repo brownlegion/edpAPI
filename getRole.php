30a72913a7886402a5e6081e4a99978811196587
Krishna Deoram <krishna.deoram@gmail.com>
2016-04-11 08:58:41 -0400

    Added the new fields.

--- getRole.php
+++ getRole.php
@@ -1,64 +1,61 @@
 <?php
 
-include 'FileMaker.php';
-include 'edplocation.php';
-
-if(isset($_REQUEST['id'])) {
-
-$id = $_REQUEST['id'];
-
+    include 'FileMaker.php';
+  include 'edplocation.php';
+  
+  if(isset($_REQUEST['id'])) {
+  
+  $id = $_REQUEST['id'];
   $fm = new FileMaker($databasename, $ip, $login, $pass);
-  $findRec = $fm->newCompoundFindCommand('acl');
-  $findReq1 = $fm->newFindRequest('acl');
-  $findReq1->addFindCriterion('userId', $id);
+  
+  $findRec = $fm->newCompoundFindCommand('roles');
+  $findReq1 = $fm->newFindRequest('roles');
+  $findReq1->addFindCriterion('id', $id);
   $findRec->add(1,$findReq1);
   $result = $findRec->execute();
   
-  if (FileMaker::isError($result)) 
-	{
+  if (FileMaker::isError($result)) {
+  
     $returnValue = 0;
 		echo $returnValue;
     exit();
+    
   } else {
   
-    $records = $result->getRecords();
-    //$permissions = array();
+  //$role = array();
+  $records = $result->getRecords();
     
     foreach ($records as $record) {
     
-    //Variable names make no sense, but produce right results.
-           $tempid = $record->getField('createUsers');
-           $tempmark = $record->getField('editUsers');
-           $updated = $record->getField('deleteUsers');
-           $updatedBy = $record->getField('viewUsers');
-           $course = $record->getField('addGrades');
-           $section = $record->getField('editGrades');
-           $section2 = $record->getField('deleteGrades');
-           $section3 = $record->getField('viewGrades');
-           $createCourses = $record->getField('createCourses');
+      //$tempid = $record->getField('id');
+      $createUsers = $record->getField('createUsers');
+      $deleteUsers = $record->getField('deleteUsers');
+      $editUsers = $record->getField('editUsers');
+      $viewUsers = $record->getField('viewUsers');
+      $createGrades = $record->getField('addGrades');
+      $deleteGrades = $record->getField('deleteGrades');
+      $editGrades = $record->getField('editGrades');
+      $viewGrades = $record->getField('viewGrades');
+      $createCourses = $record->getField('createCourses');
       $createSections = $record->getField('createSections');
       $addStudents = $record->getField('addStudents');
-           //array_push($permissions, array('createUsers'=>$tempid, 'editUsers'=>$tempmark, 'deleteUsers'=>$updated, 'viewUsers'=>$updatedBy, 
-           //'addGrades'=>$course, 'editGrades'=>$section, 'deleteGrades'=>$section2, 'viewGrades'=>$section3));
-           $permissions = array('createUsers'=>$tempid, 'editUsers'=>$tempmark, 'deleteUsers'=>$updated, 'viewUsers'=>$updatedBy, 
-           'addGrades'=>$course, 'editGrades'=>$section, 'deleteGrades'=>$section2, 'viewGrades'=>$section3,
-           'createCourses'=>$createCourses, 'createSections'=>$createSections, 'addStudents'=>$addStudents);
+      //array_push($role, array('createUesrs'=>$createUsers,'deleteUsers'=>$deleteUsers,'editUsers'=>$editUsers, 'viewUsers'=>$viewUsers, 'addGrades'=>$createGrades, 'deleteGrades'=>$deleteGrades, 'editGrades'=>$editGrades, 'viewGrades'=>$viewGrades));
+      $role = array('createUsers'=>$createUsers,'deleteUsers'=>$deleteUsers,'editUsers'=>$editUsers, 'viewUsers'=>$viewUsers, 'addGrades'=>$createGrades,
+       'deleteGrades'=>$deleteGrades, 'editGrades'=>$editGrades, 'viewGrades'=>$viewGrades, 'createCourses'=>$createCourses, 'createSections'=>$createSections, 'addStudents'=>$addStudents);
     
     }
     
-    echo json_encode($permissions);
-    //$returnValue = 1;
-    //echo $returnValue;
-    exit();
-    
-  }
+    echo json_encode($role);
 
-} else {
-    
-    $returnValue = -1;
-		echo $returnValue;
     exit();
+    }
+    
+    } else {
+    
+      echo -1;
+      exit();
+    
+    }
 
-}
 
 ?>
